102cd62682d395ae320c51706b866d8334d2b8a4
“GopiStarkk” <gopi.vjf@gmail.com>
2023-03-01 11:03:10 +0000

    Second commit from feature branch

--- gitdemocode.java
+++ gitdemocode.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;  
-import java.lang.Math;  
-import java.io.printer; 
-import java.io.collection; 
-import java.io.scanner;
-import java.io.outputprinter;
-import java.io.inputprinter;
-public class SpiralPatternExample2
+****************************SECOND VERSION OF CODE**************************************
+***************************************************************************************************
+
+
+
 {  
 //function to print the spiral pattern  
 public static void printPattern(int n)  
